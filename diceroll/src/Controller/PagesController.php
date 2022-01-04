4128eb3ec13e33f5080d60136e9ea460cc5289d8
EJaslet <contact@ericjaslet.fr>
2022-01-04 10:23:11 +0100

    Add form and redirect to first view

--- diceroll/src/Controller/PagesController.php
+++ diceroll/src/Controller/PagesController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Experience;
+use App\Form\Type\ExperienceType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,7 +14,11 @@ class PagesController extends AbstractController
     public function home(): Response
     {
         
-        return $this->render('pages/home.html.twig', [
+        $experience = new Experience();
+        $form = $this->createForm(ExperienceType::class, $experience);
+
+        return $this->render('pages/index.html.twig', [
+            'form'  => $form->createView(),
         ]);
     }
 
