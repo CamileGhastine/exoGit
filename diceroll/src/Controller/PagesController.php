f881c393c4055e53bd324faf154ba3ee32dfefbe
CamileGhastine <camileghastine@hotmail.com>
2022-01-04 11:29:11 +0100

    result in ajax

--- diceroll/src/Controller/PagesController.php
+++ diceroll/src/Controller/PagesController.php
@@ -2,35 +2,46 @@
 
 namespace App\Controller;
 
-use App\Service\ExperienceMaker;
 use App\Entity\Experience;
+use App\Service\ExperienceMaker;
 use App\Form\Type\ExperienceType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Service\CombinaisonCalculator\CombinaisonCalculator;
 use App\Service\CombinaisonCalculator\ThreeOfAKind;
+use App\Service\CombinaisonCalculator\CombinaisonCalculator;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PagesController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function home(ExperienceMaker $experienceMaker, ThreeOfAKind $threeOfAKind): Response
+    public function home(): Response
     {
 
         $experience = new Experience();
         $form = $this->createForm(ExperienceType::class, $experience);
 
-        if($form->issubmitted && $form->isValide) {
-             $experience = $experienceMaker->throwDices(5);
-
-             $result = $threeOfAKind->calcul($experience, 666);    
-             
-             dd($result);
-        }
-
         return $this->render('pages/index.html.twig', [
             'form'  => $form->createView(),
         ]);
+    }
+
+    #[Route('/ajaxResult', name: 'ajaxResult')]
+    public function ajaxResult(Request $request, ExperienceMaker $experienceMaker, ThreeOfAKind $threeOfAKind): Response
+    {
+        $number = $request->request->get('numberOfThrow');
+
+        if(is_numeric($number)) {
+            $number = intval($number);
+
+            $experience = $experienceMaker->throwDices(5);
+
+            $result = $threeOfAKind->calcul($experience, 666);    
+             
+            return $this->json([$result], 200);
+        }
+
+        return $this->json(['error' => 'Le nombre de jet doit être un entier'], 400);
     }
 
     #[Route('/description', name: 'description')]
